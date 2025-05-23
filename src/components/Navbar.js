61c4931b96e7cff67f288cd485865d070e2fadda
Divyasree-palanisamy <142414497+Divyasree-palanisamy@users.noreply.github.com>
2025-05-23 18:46:39 +0530

    Added icon

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,10 +3,21 @@ import { Link } from 'react-router-dom';
 
 function Navbar({ theme, setTheme }) {
   return (
-    <nav className="navbar">
-      <div className="navbar-left">
-        <h2>Task Manager</h2>
-      </div>
+<nav className="navbar">
+  <div className="navbar-left" style={{ display: 'flex', alignItems: 'center', gap: '10px' }}>
+    <img
+      src="/logo1.png"
+      alt="Logo"
+      style={{
+        width: '40px',
+        height: '40px',
+        borderRadius: '50%',
+        objectFit: 'cover',
+        backgroundColor: 'white',
+      }}
+    />
+    <h2 style={{ margin: 0 }}>Task Manager</h2>
+  </div>
       <div className="navbar-right">
         <button onClick={() => setTheme('default-theme')}>Default</button>
         <button onClick={() => setTheme('light-theme')}>Light</button>
